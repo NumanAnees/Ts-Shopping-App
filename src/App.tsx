fa87f7e95d7041bdc98a145adc461c2e372da9eb
Numan Anees <numananees44@gmail.com>
2022-06-24 16:39:11 +0500

    Navbar Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./Pages/Home";
+import Store from "./Pages/Store";
+import About from "./Pages/About";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </>
+  );
 };
 
 export default App;
